1dacfb5218b5b08b347adb2a586ca697fc5123c1
mohsin-tab <mm9105996713@gmail.com>
2026-02-24 10:50:22 +0530

    change

--- src/types/express.d.ts
+++ src/types/express.d.ts
@@ -1,10 +1,13 @@
-import "express";
 
-declare module "express-serve-static-core" {
-  interface Request {
-    user?: {
-      userId: string;
-      role: string;
-    };
+declare global {
+  namespace Express {
+    interface Request {
+      user?: {
+        userId: string;
+        role: string;
+      };
+    }
   }
 }
+
+export {};
